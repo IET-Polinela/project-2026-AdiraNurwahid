021819bb8caa6a45aeeb21e90ab0447f3cd3737b
Adira Nur Wahid <adiranurwahid@gmail.com>
2026-07-01 22:55:15 +0700

    perubahan app.js 2

--- server_smartcity/smartcity_app/settings.py
+++ server_smartcity/smartcity_app/settings.py
@@ -170,6 +170,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "https://iet-polinela.github.io",
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
